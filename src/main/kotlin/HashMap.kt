f61d59c9d1002efbe678593c611d45adfa306173
Paxagi <paxagi@yandex.ru>
2023-12-25 18:15:47 +0300

    add: add push function and hide dict variable

--- src/main/kotlin/HashMap.kt
+++ src/main/kotlin/HashMap.kt
@@ -1,12 +1,17 @@
 import java.security.MessageDigest
 
 class HashMap <K, V> (key: K, value: V) {
-    var dict = emptyArray<List<Pair<K, V>>>()
+    private var dict = emptyArray<List<Pair<K, V>>>()
     fun hash(key: K): String {
         return MessageDigest
             .getInstance("SHA-256")
             .digest(key.toString().toByteArray())
             .fold("", { str, it -> str + "%02x".format(it) })
+    }
+
+    fun push(key: K, value: V) {
+        val bucket = hash(key)
+        //todo: побайтово конвертировать строку в число
     }
 
 }
