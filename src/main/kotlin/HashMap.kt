26e36a1f2e11597ea550c4b88dbccadacd71128b
Paxagi <paxagi@yandex.ru>
2023-12-25 17:33:33 +0300

    add: structure type declaration

--- src/main/kotlin/HashMap.kt
+++ src/main/kotlin/HashMap.kt
@@ -1,7 +1,7 @@
 import java.security.MessageDigest
 
 class HashMap <K, V> (key: K, value: V) {
-
+    var dict = emptyArray<List<Pair<K, V>>>()
     fun hash(key: K): String {
         return MessageDigest
             .getInstance("SHA-256")
