f9b0b10df0d149a6a9872ca81bab61e4badf4e8e
Paxagi <paxagi@yandex.ru>
2024-01-21 21:04:11 +0300

    ref: HashMap: add size property and its extensibility

--- src/main/kotlin/HashMap.kt
+++ src/main/kotlin/HashMap.kt
@@ -5,6 +5,9 @@ class MyHashMap <K, V> {
         val hash = key.hashCode()
         var next: Node<K, V>? = null
     }
+
+    var size: Int = 0
+        private set
     private var dict = Array<LinkedList<Node<K, V>>?>(DEFAULT_SIZE) { null }
 
     companion object {
@@ -29,18 +32,23 @@ class MyHashMap <K, V> {
 
     fun add(key: K, value: V) {
         val bucketIndex = key.hashCode() % DEFAULT_SIZE
-        val nodesList = dict[bucketIndex]
+        val nodesList: LinkedList<Node<K, V>>? = dict[bucketIndex]
 
-        if (nodesList.isNullOrEmpty()) {
-            dict[bucketIndex] = LinkedList<Node<K, V>>().also { it.add(Node(key, value)) }
+        if (nodesList.isNullOrEmpty()) { // new list and node
+            dict[bucketIndex] = LinkedList<Node<K, V>>()
+                .also { it.add(Node(key, value)) }
+                .also { size++ }
         } else {
             val node: Node<K, V>? = keyToNode(key)
-            if (node == null) {
-                nodesList.add(Node(key, value))
-            } else {
+            if (node == null) { // new node
+                nodesList
+                    .add(Node(key, value))
+                    .also { size++ }
+            } else { // rewrite a node
                 node.value = value
             }
         }
+
     }
 
     operator fun get(key: K): V? = keyToNode(key)?.value
@@ -54,4 +62,5 @@ fun main() {
     println(myHashMap[1])
     myHashMap.add(2, "it's two")
     println(myHashMap[2])
+    println(myHashMap.size)
 }
